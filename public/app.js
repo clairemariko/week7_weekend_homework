592030ed49200c6239cc7160378b59272f9d1aa4
claire <claire.mariko.smith@gmail.com>
2016-04-03 21:29:26 +0100

    weekend homework start

---
+++ public/app.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    var url = 'http://swapi.co/api/planets/'
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onload = function () {
+        if (request.status === 200) {
+            var jsonString = request.responseText;
+            var countries = JSON.parse(jsonString);
+            main(countries);
+        }
+    }
+    request.send(null);
+};
